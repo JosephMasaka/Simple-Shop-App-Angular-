4b784f71a03cb8a187007b6833c646754fd79044
JosephMasaka <masakajoseph14@gmail.com>
2025-01-24 14:07:32 +0300

    section for showing various products in certain category(home page)

--- src/app/home/components/home-page/home-page.component.ts
+++ src/app/home/components/home-page/home-page.component.ts
@@ -16,13 +16,19 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class HomePageComponent implements OnInit {
   categories: string[] = [];
+  electronics: any[] = [];
+  jewellery: any[] = [];
   isLoading: boolean = true;
+  isLoadingElectronics: boolean = true;
+  isLoadingJewellery: boolean = true;
   error: string | null = null;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.fetchCategories();
+    this.fetchCategoryElectronics();
+    this.fetchCategoryJewellery();
   }
 
   fetchCategories(): void {
@@ -39,4 +45,57 @@ export class HomePageComponent implements OnInit {
       },
     });
   }
+
+  fetchCategoryElectronics(): void {
+    const apiUrl = 'https://fakestoreapi.com/products/category/electronics';
+  
+    this.http.get<any[]>(apiUrl).subscribe({
+      next: (response) => {
+        this.electronics = response;
+        this.isLoadingElectronics = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to fetch electronics category.';
+        this.isLoadingElectronics = false;
+      },
+    });
+  }
+  
+  fetchCategoryJewellery(): void {
+    const apiUrl = 'https://fakestoreapi.com/products/category/jewelery';
+  
+    this.http.get<any[]>(apiUrl).subscribe({
+      next: (response) => {
+        this.jewellery = response;
+        this.isLoadingJewellery = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to fetch jewellery category.';
+        this.isLoadingJewellery = false;
+      },
+    });
+  }
+  
+
+  // Fetch products by category
+  // fetchProductsByCategory(category: string): void {
+  //   const apiUrl = `https://fakestoreapi.com/products/category/electronics`;
+  //   this.http.get<any[]>(apiUrl).subscribe({
+  //     next: (response) => {
+  //       if (category === 'electronics') {
+  //         this.electronics = response;
+  //         this.isLoadingElectronics = false;
+  //       },
+  //       error: (err: any) => {
+  //         this.error = 'Failed to fetch categories.';
+  //         this.isLoading = false;
+  //       },
+  //     },
+  //     error: () => {
+  //       this.error = `Failed to fetch products for ${category}.`;
+  //       if (category === 'electronics') this.isLoadingElectronics = false;
+  //       if (category === 'jewelery') this.isLoadingJewellery = false;
+  //     }
+  //   });
+  // }
 }
